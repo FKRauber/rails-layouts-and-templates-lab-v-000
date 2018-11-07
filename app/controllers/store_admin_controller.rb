0db3de5680131f98e17aa42433ccde6a8965852c
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-11-07 17:33:33 +0000

    automatically backed up by learn

--- app/controllers/store_admin_controller.rb
+++ app/controllers/store_admin_controller.rb
@@ -1,15 +1,13 @@
 class StoreAdminController < ApplicationController
   layout "admin"
-
   def home
   end
-  def admin
+   def admin
   end
-
-  def orders
+   def orders
     render :layout => "order_administration"
   end
-  def invoice
+   def invoice
     render :layout => false
   end
 end
